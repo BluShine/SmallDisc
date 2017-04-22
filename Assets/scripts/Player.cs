d70fa7f6852dc7f2832131a923a0436442679cdc
Mark <umbreon27@gmail.com>
2017-04-21 19:47:23 -0700

    Player can move around on a disc.
    Initial Unity project with a cube that can move around on a disc.

---
+++ Assets/scripts/Player.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public float moveForce;
+    public float maxSpeed;
+
+    Rigidbody body;
+
+	// Use this for initialization
+	void Start () {
+        body = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void FixedUpdate()
+    {
+        Vector3 input = new Vector3(-Input.GetAxis("Horizontal"), 0, -Input.GetAxis("Vertical"));
+        if(input.magnitude > 1) { input = input.normalized; }
+        if (body.velocity.magnitude < maxSpeed)
+        {
+            body.AddForce(input * moveForce);
+        }
+    }
+}
